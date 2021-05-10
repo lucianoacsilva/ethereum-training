4b2391db7276eeb4ec32dc6533a1372c5f233bec
Luciano Augusto Campagnoli da Silva <lucianos@cpqd.com.br>
2021-05-10 18:16:34 -0300

    Moves project of basic training to specific folder

---
+++ basic-training/migrations/1619794777_truffle_contract.js
@@ -0,0 +1,6 @@
+const TruffleContract = artifacts.require("./TruffleContract.sol");
+
+module.exports = function(_deployer) {
+  // Use deployer to state migration tasks.
+  _deployer.deploy(TruffleContract);
+};
